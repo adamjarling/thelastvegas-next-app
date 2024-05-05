bd715490f796cc6fc79ce58474e9fe7a7a0b3de5
Adam Joseph Arling <aarling@gmail.com>
2024-05-05 18:03:15 -0500

    Update Live page with Summerfest announcement and new banner image

--- app/live/page.tsx
+++ app/live/page.tsx
@@ -1,17 +1,32 @@
 import Banner from "@/components/banner/Banner";
 import BannerHeadline from "@/components/banner/Headline";
+import BannerWrapper from "@/components/live/BannerWrapper";
+import Image from "next/image";
 import Main from "@/components/Main";
 import Obfuscate from "@/components/Obfuscate";
+import summerFest from "public/images/tlv_summerfest_2024.jpeg";
+import tkoLogo from "public/images/TKO-logo.png";
 
 const ShowsPage = () => {
   return (
-    <Main>
+    <Main flushTop>
+      <BannerWrapper />
       <Banner className="md:py-20">
         <BannerHeadline>Live</BannerHeadline>
       </Banner>
-      <p className="mb-20 text-center">
-        Booking: <Obfuscate />
-      </p>
+      <div className="max-w-3xl mx-auto">
+        <Image src={summerFest} alt="TLV Summerfest 2024" />
+      </div>
+
+      <section className="mt-24 text-center">
+        <h2 className="mb-10 text-6xl uppercase">Booking</h2>
+        <p className="mb-5 text-center">
+          Booking: <Obfuscate />
+        </p>
+        <div className="flex justify-center mb-20">
+          <Image src={tkoLogo} alt="TKO logo" />
+        </div>
+      </section>
     </Main>
   );
 };
---
+++ components/live/BannerWrapper.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import ParallaxHero from "../ParallaxHero";
+import React from "react";
+
+const BannerWrapper = () => {
+  return (
+    <ParallaxHero
+      imageUrl="/photos/l_6c0ff513ec9abc0d2ae1673172642984.jpg"
+      headline="Summerfest 2024 just announced"
+      altText="The Last Vegas live at the Rockford Speedway"
+    />
+  );
+};
+
+export default BannerWrapper;
